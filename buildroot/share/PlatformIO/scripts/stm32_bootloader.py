cebe259bb4cebf0f1de99ba7e5015fd527e89249
langgo <137526291@qq.com>
2021-05-06 00:16:16 +0800

    NEW: add vector offset compile flag. we will use boot loader!

--- buildroot/share/PlatformIO/scripts/stm32_bootloader.py
+++ buildroot/share/PlatformIO/scripts/stm32_bootloader.py
@@ -10,6 +10,25 @@ def noencrypt(source, target, env):
   if not os.path.isfile(firmware):
     shutil.copy(target[0].path, firmware)
 
+print('langgo: stm32 bootloader py execute')
+
+for define in env['CPPDEFINES']:
+    LD_FLASH_OFFSET = ''
+    if define[0] == "VECT_TAB_OFFSET":
+        LD_FLASH_OFFSET = define[1]
+        print('langgo: find define.vect tab offset:' + LD_FLASH_OFFSET)
+    maximum_ram_size = board.get("upload.maximum_ram_size")
+
+    for i, flag in enumerate(env["LINKFLAGS"]):
+      if "-Wl,--defsym=LD_FLASH_OFFSET" in flag:
+        env["LINKFLAGS"][i] = "-Wl,--defsym=LD_FLASH_OFFSET=" + LD_FLASH_OFFSET
+      if "-Wl,--defsym=LD_MAX_DATA_SIZE" in flag:
+        env["LINKFLAGS"][i] = "-Wl,--defsym=LD_MAX_DATA_SIZE=" + str(maximum_ram_size - 40)
+
+    # if 'firmware' in board.get("build").keys():
+    #   env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", noencrypt);
+        
+
 if 'offset' in board.get("build").keys():
   LD_FLASH_OFFSET = board.get("build.offset")
 
@@ -17,6 +36,7 @@ if 'offset' in board.get("build").keys():
     if define[0] == "VECT_TAB_OFFSET":
       env['CPPDEFINES'].remove(define)
   env['CPPDEFINES'].append(("VECT_TAB_OFFSET", LD_FLASH_OFFSET))
+  print('langgo: vect tab offset' + LD_FLASH_OFFSET)
 
   maximum_ram_size = board.get("upload.maximum_ram_size")
 
