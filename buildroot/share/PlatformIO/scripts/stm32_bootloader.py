abb342371d0574ad050322cb3a4cf354f6715384
杜建树 <3008993280@qq.com>
2023-06-05 14:41:30 +0800

    Fix:"RuntimeError: deque mutated during iteration:" error.

--- buildroot/share/PlatformIO/scripts/stm32_bootloader.py
+++ buildroot/share/PlatformIO/scripts/stm32_bootloader.py
@@ -10,33 +10,48 @@ def noencrypt(source, target, env):
   if not os.path.isfile(firmware):
     shutil.copy(target[0].path, firmware)
 
-print('langgo: stm32 bootloader py execute')
+print('dashu: stm32 bootloader py execute')
 
-for define in env['CPPDEFINES']:
-    LD_FLASH_OFFSET = ''
-    if define[0] == "VECT_TAB_OFFSET":
-        LD_FLASH_OFFSET = define[1]
-        print('langgo: find define.vect tab offset:' + LD_FLASH_OFFSET)
-    maximum_ram_size = board.get("upload.maximum_ram_size")
+# for define in env['CPPDEFINES']:
+#   LD_FLASH_OFFSET = ''
+#   if define[0] == "VECT_TAB_OFFSET":
+#     LD_FLASH_OFFSET = define[1]
+#     print('langgo: find define.vect tab offset:' + LD_FLASH_OFFSET)
+#   maximum_ram_size = board.get("upload.maximum_ram_size")
 
-    for i, flag in enumerate(env["LINKFLAGS"]):
-      if "-Wl,--defsym=LD_FLASH_OFFSET" in flag:
-        env["LINKFLAGS"][i] = "-Wl,--defsym=LD_FLASH_OFFSET=" + LD_FLASH_OFFSET
-      if "-Wl,--defsym=LD_MAX_DATA_SIZE" in flag:
-        env["LINKFLAGS"][i] = "-Wl,--defsym=LD_MAX_DATA_SIZE=" + str(maximum_ram_size - 40)
+# if 'offset' in board.get("build").key():
+#   LD_FLASH_OFFSET = board.get("build.offset")
+#   print('dashu: find define.vect tab offset:' + LD_FLASH_OFFSET)
+#   maximum_ram_size = board.get("upload.maximum_ram_size")
 
-    # if 'firmware' in board.get("build").keys():
-    #   env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", noencrypt);
-        
+#   for i, flag in enumerate(env["LINKFLAGS"]):
+#     if "-Wl,--defsym=LD_FLASH_OFFSET" in flag:
+#       env["LINKFLAGS"][i] = "-Wl,--defsym=LD_FLASH_OFFSET=" + LD_FLASH_OFFSET
+#     if "-Wl,--defsym=LD_MAX_DATA_SIZE" in flag:
+#       env["LINKFLAGS"][i] = "-Wl,--defsym=LD_MAX_DATA_SIZE=" + str(maximum_ram_size - 40)
+
+  # if 'firmware' in board.get("build").keys():
+  #   env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", noencrypt);
+
 # 'offset' property defined in buildroot\share\PlatformIO\boards\langgo407ve.json
 if 'offset' in board.get("build").keys():
   LD_FLASH_OFFSET = board.get("build.offset")
 
+  to_remove = []
+  to_append = []
   for define in env['CPPDEFINES']:
     if define[0] == "VECT_TAB_OFFSET":
-      env['CPPDEFINES'].remove(define)
-  env['CPPDEFINES'].append(("VECT_TAB_OFFSET", LD_FLASH_OFFSET))
-  print('langgo: vect tab offset' + LD_FLASH_OFFSET)
+      to_remove.append(define)
+      to_append.append(("VECT_TAB_OFFSET", LD_FLASH_OFFSET))
+  for define in to_remove:
+    env['CPPDEFINES'].remove(define)
+  for define in to_append:
+    env['CPPDEFINES'].append(define)
+  # for define in env['CPPDEFINES']:
+  #   if define[0] == "VECT_TAB_OFFSET":
+  #     env['CPPDEFINES'].remove(define)
+  # env['CPPDEFINES'].append(("VECT_TAB_OFFSET", LD_FLASH_OFFSET))
+  print('dashu: vect tab offset' + LD_FLASH_OFFSET)
 
   maximum_ram_size = board.get("upload.maximum_ram_size")
 
